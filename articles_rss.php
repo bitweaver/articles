a8163cf825ba4f49c2e05a02d099868cc734450b
Max Kremmel <xing@synapse.plus.com>
2005-10-29 10:02:03 +0000

    clean up rss stuff further and return more data - parsed data for articles, blogs and wiki

--- articles_rss.php
+++ articles_rss.php
@@ -6,33 +6,36 @@ require_once( ARTICLES_PKG_PATH."BitArticle.php" );
 $gBitSystem->verifyPackage( 'articles' );
 $gBitSystem->verifyPackage( 'rss' );
 
-$rss->title = $gBitSystem->getPreference( 'title_rss_articles', $gBitSystem->mPrefs['siteTitle'] );
+$rss->title = $gBitSystem->getPreference( 'title_rss_articles', $gBitSystem->mPrefs['siteTitle'].' - '.tra( 'Articles' ) );
 $rss->description = $gBitSystem->getPreference( 'desc_rss_articles', $gBitSystem->mPrefs['siteTitle'].' - '.tra( 'RSS Feed' ) );
 
 // check permission to view articles
 if( !$gBitUser->hasPermission( 'bit_p_read_article' ) ) {
 	require_once( RSS_PKG_PATH."rss_error.php" );
 } else {
+	// check if we want to use the cache file
+	$cacheFile = TEMP_PKG_PATH.RSS_PKG_NAME.'/'.ARTICLES_PKG_NAME.'_'.$version.'.xml';
+	$rss->useCached( $cacheFile ); // use cached version if age < 1 hour
+
 	$articles = new BitArticle();
 	$listHash = array(
 		'sort_mode' => 'publish_date_desc',
 		'max_records' => $gBitSystem->getPreference( 'max_rss_articles', 10 ),
 	);
 	$feeds = $articles->getList( $listHash );
-	$feeds = $feeds['data'];
 
 	// get all the data ready for the feed creator
-	foreach( $feeds as $feed ) {
+	foreach( $feeds['data'] as $feed ) {
 		$item = new FeedItem();
 		$item->title = $feed['title'];
-		$item->link = 'http://'.$_SERVER['HTTP_HOST'].BIT_ROOT_URL.$articles->getDisplayUrl( $feed['title'] );
-		$item->description = $feed['data'];
+		$item->link = BIT_BASE_URI.$articles->getDisplayUrl( $feed['title'] );
+		$item->description = $feed['parsed_data'];
 
-		$item->date = (int) $feed['publish_date'];
+		$item->date = ( int )$feed['publish_date'];
 		$item->source = 'http://'.$_SERVER['HTTP_HOST'].BIT_ROOT_URL;
 		$item->author = $feed['author_name'];
 
-		$item->descriptionTruncSize = $gBitSystem->getPreference( 'rssfeed_truncate', 500 );
+		$item->descriptionTruncSize = $gBitSystem->getPreference( 'rssfeed_truncate', 1000 );
 		$item->descriptionHtmlSyndicated = FALSE;
 
 		// pass the item on to the rss feed creator
@@ -40,8 +43,6 @@ if( !$gBitUser->hasPermission( 'bit_p_read_article' ) ) {
 	}
 
 	// finally we are ready to serve the data
-	$cacheFile = TEMP_PKG_PATH.'rss/articles_'.$version.'.xml';
-	$rss->useCached( $cacheFile ); // use cached version if age < 1 hour
 	echo $rss->saveFeed( $rss_version_name, $cacheFile );
 }
 ?>

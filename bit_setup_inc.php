f95b808902c10f8de6abefce8d3911f9b86489a6
Max Kremmel <xing@synapse.plus.com>
2005-12-10 10:53:26 +0000

    use PKG_DIR as key for registerAppMenu

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -3,7 +3,7 @@ global $gBitSystem, $gBitUser, $gBitSmarty;
 $gBitSystem->registerPackage( 'articles', dirname( __FILE__ ).'/' );
 
 if( $gBitSystem->isPackageActive( 'articles' ) ) {
-	$gBitSystem->registerAppMenu( 'articles', 'Articles', ARTICLES_PKG_URL.'index.php', 'bitpackage:articles/menu_articles.tpl', 'articles' );
+	$gBitSystem->registerAppMenu( ARTICLES_PKG_DIR, 'Articles', ARTICLES_PKG_URL.'index.php', 'bitpackage:articles/menu_articles.tpl', ARTICLES_PKG_NAME );
 
 	$gBitSystem->registerNotifyEvent( array( "article_submitted" => tra( "A user submits an article" ) ) );
 }

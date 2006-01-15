a97a6582cb368c254010f5b3c763ea1521af056f
Christian Fowler <spider@viovio.com>
2006-01-15 15:33:54 +0000

    fix registerAppMenu call so it follows renaming of directory

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -3,7 +3,7 @@ global $gBitSystem, $gBitUser, $gBitSmarty;
 $gBitSystem->registerPackage( 'articles', dirname( __FILE__ ).'/' );
 
 if( $gBitSystem->isPackageActive( 'articles' ) ) {
-	$gBitSystem->registerAppMenu( ARTICLES_PKG_DIR, 'Articles', ARTICLES_PKG_URL.'index.php', 'bitpackage:articles/menu_articles.tpl', ARTICLES_PKG_NAME );
+	$gBitSystem->registerAppMenu( ARTICLES_PKG_NAME, ucfirst( ARTICLES_PKG_DIR ), ARTICLES_PKG_URL.'index.php', 'bitpackage:articles/menu_articles.tpl', ARTICLES_PKG_NAME );
 
 	$gBitSystem->registerNotifyEvent( array( "article_submitted" => tra( "A user submits an article" ) ) );
 }

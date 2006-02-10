580f260774c6ad946643927eb880ed660ece3073
Lester Caine <lester@lsces.co.uk>
2006-02-10 07:25:02 +0000

    Move package guid define to allow multi-package access without class

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -2,6 +2,8 @@
 global $gBitSystem, $gBitUser, $gBitSmarty;
 $gBitSystem->registerPackage( 'articles', dirname( __FILE__ ).'/' );
 
+define( 'BITARTICLE_CONTENT_TYPE_GUID', 'bitarticle' );
+
 if( $gBitSystem->isPackageActive( 'articles' ) ) {
 	$gBitSystem->registerAppMenu( ARTICLES_PKG_NAME, ucfirst( ARTICLES_PKG_DIR ), ARTICLES_PKG_URL.'index.php', 'bitpackage:articles/menu_articles.tpl', ARTICLES_PKG_NAME );
 

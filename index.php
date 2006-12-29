b0ebcc03366b0c18efffd70c371f7e53192b4062
Max Kremmel <xing@synapse.plus.com>
2006-12-29 10:38:03 +0000

    add missing tra()

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_articles/index.php,v 1.18 2006/08/29 16:46:45 hash9 Exp $
+// $Header: /cvsroot/bitweaver/_bit_articles/index.php,v 1.19 2006/12/29 10:38:03 squareing Exp $
 // Copyright( c )2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -12,6 +12,12 @@ $gBitSystem->verifyPackage( 'articles' );
 
 // Now check permissions to access this page
 $gBitSystem->verifyPermission( 'p_articles_read' );
+
+// {{{ FCAMOD
+//if( $gBitUser->isRegistered() ) {
+	$gBitSystem->setConfig( 'articles_hide_right_col', 'n' );
+//}
+// FCAMOD }}}
 
 if( !empty( $_REQUEST['article_id'] ) ) {
 	header( "location: ".ARTICLES_PKG_URL."read.php?article_id=".( ( int )$_REQUEST['article_id'] ) );
@@ -48,5 +54,5 @@ $gDefaultCenter = 'bitpackage:articles/center_list_articles.tpl';
 $gBitSmarty->assign_by_ref( 'gDefaultCenter', $gDefaultCenter );
 
 // Display the template
-$gBitSystem->display( 'bitpackage:kernel/dynamic.tpl', 'Articles' );
+$gBitSystem->display( 'bitpackage:kernel/dynamic.tpl', tra( 'Articles' ) );
 ?>

2e452f22607e2bb461ada2e68cf883cb1ea69ecd
wjames5 <will@tekimaki.com>
2007-06-15 20:42:22 +0000

    oops clean out commented old version of file

--- templates/center_list_articles.php
+++ templates/center_list_articles.php
@@ -1,32 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_articles/templates/center_list_articles.php,v 1.9 2007/06/15 20:14:10 wjames5 Exp $
-/*
-require_once( '../bit_setup_inc.php' );
-include_once( ARTICLES_PKG_PATH . 'BitArticle.php' );
-
-$gBitSystem->verifyPackage( 'articles' );
-
-$gBitSystem->verifyPermission( 'p_articles_read' );
-
-include_once(ARTICLES_PKG_PATH."lookup_article_inc.php");
-
-if ( isset( $_REQUEST["remove"] ) ) {
-    $gBitSystem->verifyPermission( 'p_articles_remove' );
-}
-
-if (empty($_REQUEST['status_id']) || (!$gBitUser->hasPermission('p_articles_approve_submission') && !$gBitUser->hasPermission('p_articles_admin'))) {
-	$_REQUEST['status_id'] = ARTICLE_STATUS_APPROVED;
-}
-
-$gBitSmarty->assign('descriptionLength', $gBitSystem->getConfig( 'articles_description_length', 500 ) );
-$gBitSmarty->assign('showDescriptionsOnly', TRUE);
-
-if( empty( $articles ) ) {
-	$articles = $gContent->getList($_REQUEST);
-	$gBitSmarty->assign_by_ref('articles', $articles);
-}
-*/
-
+// $Header: /cvsroot/bitweaver/_bit_articles/templates/center_list_articles.php,v 1.10 2007/06/15 20:42:22 wjames5 Exp $
 global $gBitSmarty, $gContent, $gBitSystem, $gQueryUserId, $moduleParams;
 extract( $moduleParams );
 

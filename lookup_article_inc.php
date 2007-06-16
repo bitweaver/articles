ddaac11bfae9cdc96df9356a308acf54f96c8379
Max Kremmel <xing@synapse.plus.com>
2007-06-16 00:00:26 +0000

    indentation

--- lookup_article_inc.php
+++ lookup_article_inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_articles/lookup_article_inc.php,v 1.8 2006/01/14 19:53:53 squareing Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_articles/lookup_article_inc.php,v 1.9 2007/06/16 00:00:26 squareing Exp $
  * @package article
  * @subpackage functions
  */
@@ -8,27 +8,26 @@
 /**
  * Initialization
  */
-	global $gContent;
-	require_once( ARTICLES_PKG_PATH.'BitArticle.php');
-	require_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
+require_once( ARTICLES_PKG_PATH.'BitArticle.php');
+require_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
 
-	// if we already have a gContent, we assume someone else created it for us, and has properly loaded everything up.
-	if( empty( $gContent ) || !is_object( $gContent ) ) {
-		if (@BitBase::verifyId( $_REQUEST['article_id'] ) ) {
-			$gContent = new BitArticle( $_REQUEST['article_id'] );
-		} elseif( @BitBase::verifyId( $_REQUEST['content_id'] ) ) {
-			$gContent = new BitArticle( NULL, $_REQUEST['content_id'] );
-		} else {
-			$gContent = new BitArticle();
-			$gContent->mInfo['expire_date'] = strtotime( "+1 year" );
-		}
-
-		if( empty( $gContent->mArticleId ) && empty( $gContent->mContentId )  ) {
-			//handle legacy forms that use plain 'article' form variable name
-		} else {
-			$gContent->load();
-		}
-		$gBitSmarty->clear_assign( 'gContent' );
-		$gBitSmarty->assign( 'gContent', $gContent );
+// if we already have a gContent, we assume someone else created it for us, and has properly loaded everything up.
+if( empty( $gContent ) || !is_object( $gContent ) ) {
+	if (@BitBase::verifyId( $_REQUEST['article_id'] ) ) {
+		$gContent = new BitArticle( $_REQUEST['article_id'] );
+	} elseif( @BitBase::verifyId( $_REQUEST['content_id'] ) ) {
+		$gContent = new BitArticle( NULL, $_REQUEST['content_id'] );
+	} else {
+		$gContent = new BitArticle();
+		$gContent->mInfo['expire_date'] = strtotime( "+1 year" );
 	}
+
+	if( empty( $gContent->mArticleId ) && empty( $gContent->mContentId )  ) {
+		//handle legacy forms that use plain 'article' form variable name
+	} else {
+		$gContent->load();
+	}
+	$gBitSmarty->clear_assign( 'gContent' );
+	$gBitSmarty->assign( 'gContent', $gContent );
+}
 ?>

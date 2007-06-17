81883d26145c8a40d9120cdc4e1eee004e994617
Lester Caine <lester@lsces.co.uk>
2007-06-17 21:05:22 +0000

    articles_content_attachment should have been commented for the moment The articles_attachments control now actually works!

--- admin/admin_articles_inc.php
+++ admin/admin_articles_inc.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_articles/admin/admin_articles_inc.php,v 1.20 2007/06/17 19:38:33 lsces Exp $
+// $Header: /cvsroot/bitweaver/_bit_articles/admin/admin_articles_inc.php,v 1.21 2007/06/17 21:05:22 lsces Exp $
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -15,10 +15,11 @@ $formCmsSettings = array(
 		'label' => 'File Attachments',
 		'note' => 'Allow the attachment of files to an article.',
 	),
-	'articles_content_attachments'  => array(
+/*	'articles_content_attachments'  => array(
 		'label' => 'Content Attachments',
 		'note' => 'Allow the attachment of content to an article.',
 	),
+*/
 	'articles_display_filter_bar' => array(
 		'label' => 'Articles Filter',
 		'note' => 'Allows admins to quickly filter articles based on status, topic and type.',

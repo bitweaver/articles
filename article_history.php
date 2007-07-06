20343087f191d1b9f7669db2f3cc2cf4a1598051
Max Kremmel <xing@synapse.plus.com>
2007-07-06 16:10:36 +0000

    remove commented code

--- article_history.php
+++ article_history.php
@@ -44,73 +44,8 @@ if( $gContent->getField( 'status_id' ) != ARTICLE_STATUS_APPROVED && !( $gBitUse
 }
 
 $smartyContentRef = 'article';
-
 include_once( LIBERTY_PKG_PATH.'content_history_inc.php' );
 
-/* SLATED FOR REMOVAL - SCRIPT MOVED TO LIBERTY:content_history_inc.php
-$gBitSmarty->assign('source', 0);
-// If we have to include a preview please show it
-$gBitSmarty->assign('preview', false);
-$gBitSmarty->assign('compare', 'n');
-$gBitSmarty->assign('diff2', 'n');
-if (isset($_REQUEST["delete"]) && isset($_REQUEST["hist"])) {
-	foreach (array_keys($_REQUEST["hist"])as $version) {
-		$gContent->expungeVersion( $version );
-	}
-} elseif (isset($_REQUEST['source'])) {
-	$gBitSmarty->assign('source', $_REQUEST['source']);
-	if ($_REQUEST['source'] == 'current') {
-		$gBitSmarty->assign('sourcev', nl2br(htmlentities($gContent->mInfo['data'])));
-	} else {
-		$version = $gContent->getHistory($_REQUEST["source"]);
-		$gBitSmarty->assign('sourcev', nl2br(htmlentities($version[0]["data"])));
-	}
-} elseif (isset($_REQUEST["preview"])) {
-	if( $version = $gContent->load($_REQUEST["preview"])) {
-		$gContent->parseData();
-		//$gBitSmarty->assign_by_ref('parsed', $gContent->parseData( $version[0] ) );
-		$gBitSmarty->assign_by_ref('version', $_REQUEST["preview"]);
-		$gBitSmarty->assign_by_ref('article', $gContent->mInfo );
-
-	}
-} elseif( isset( $_REQUEST["diff2"] ) ) {
-	$from_version = $_REQUEST["diff2"];
-	$from_page = $gContent->getHistory( $from_version );
-	$from_lines = explode("\n",$from_page[0]["data"]);
-	$to_version = $gContent->mInfo["version"];
-	$to_lines = explode("\n",$gContent->mInfo["data"]);
-
-	include_once( WIKI_PKG_PATH.'diff.php');
-	$diffx = new WikiDiff($from_lines,$to_lines);
-	$fmt = new WikiUnifiedDiffFormatter;
-	$html = $fmt->format($diffx, $from_lines);
-	$gBitSmarty->assign('diffdata', $html);
-	$gBitSmarty->assign('diff2', 'y');
-	$gBitSmarty->assign('version_from', $from_version);
-	$gBitSmarty->assign('version_to', $to_version);
-
-} elseif( isset( $_REQUEST["compare"] ) ) {
-	$from_version = $_REQUEST["compare"];
-	$from_page = $gContent->getHistory($from_version);
-	$gBitSmarty->assign('compare', 'y');
-	$gBitSmarty->assign_by_ref('diff_from', $gContent->parseData( $from_page[0] ) );
-	$gBitSmarty->assign_by_ref('diff_to', $gContent->parseData() );
-	$gBitSmarty->assign_by_ref('version_from', $from_version);
-} elseif (isset($_REQUEST["rollback"])) {
-	if( $version = $gContent->getHistory( $_REQUEST["preview"] ) ) {
-		$gBitSmarty->assign_by_ref('parsed', $gContent->parseData( $version[0] ) );
-		$gBitSmarty->assign_by_ref('version', $_REQUEST["preview"]);
-	}
-}
-*/
-
-// pagination stuff
-/* SLATED FOR REMOVAL - WE ARE GOING TO MAKE THIS JUST LIKE WIKI TO KEEP IT SIMPLE
-$gBitSmarty->assign( 'page', $page = !empty( $_REQUEST['page'] ) ? $_REQUEST['page'] : 1 );
-$offset = ( $page - 1 ) * $gBitSystem->getConfig( 'max_records' );
-$history = $gContent->getHistory( NULL, NULL, $offset, $gBitSystem->getConfig( 'max_records' ) );
-$gBitSmarty->assign_by_ref( 'history', $history );
-*/
 $gBitSmarty->assign( 'page', $page = !empty( $_REQUEST['list_page'] ) ? $_REQUEST['list_page'] : 1 );
 $offset = ( $page - 1 ) * $gBitSystem->getConfig( 'max_records' );
 $history = $gContent->getHistory( NULL, NULL, $offset, $gBitSystem->getConfig( 'max_records' ) );

a88469351d9667b157ed36476d4681e9ab3ca58f
lsces <lester@lsces.co.uk>
2013-07-12 23:20:33 +0100

    Closing bracket missmatch fixed

--- modules/mod_articles.php
+++ modules/mod_articles.php
@@ -51,7 +51,7 @@ if( ( !empty( $module_params['topic_id'] ) || !empty( $module_params['topic_name
 	$_template->tpl_vars['moduleTitle'] = new Smarty_variable( "Articles" );
 }
 
-$_template->tpl_vars['params'] = new Smarty_variable( !empty( $moduleParams['params'] );
+$_template->tpl_vars['params'] = new Smarty_variable( !empty( $moduleParams['params'] ) );
 $_template->tpl_vars['listtype'] = new Smarty_variable(  ( isset($module_params['list_type']) && (strncasecmp($module_params['list_type'], 'u', 1) == 0) ) ? 'ul' : 'ol' );
 $_template->tpl_vars['modArticles'] = new Smarty_variable( $articles );
 ?>
